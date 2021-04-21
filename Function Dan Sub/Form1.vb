0082ffb93e0047ab2b764c43acb06946b31b577f
Rigneswari A P Visuvanathan <18DDT19F1078@student.ptss.edu.my>
2021-04-21 11:11:10 +0800

    Latest Function

--- Function Dan Sub/Form1.vb
+++ Function Dan Sub/Form1.vb
@@ -1,23 +1,36 @@
 ﻿Public Class Form1
+    Private Sub S1()
+        lblText1.Text = "Good Morning"
+    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        lblText1.Text = "Welcome To My Application"
+        S1()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub S2()
         Dim strText As String
         strText = TextBox1.Text
         Label2.Text = strText
     End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        S2()
+    End Sub
 
+    Private Function F1()
+        Return "Hello Have A Nice Day"
+    End Function
     Private Function Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Label3.Text = "DDT4C"
-        Return Label3
+        Label3.Text = F1()
+    End Function
+
+    Private Function F2()
+        Dim strText2 As String
+        strText2 = TextBox2.Text
+        Return strText2
     End Function
 
     Private Function Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Dim strText2 As String
-        strText2 = TextBox2.Text
-        Label4.Text = strText2
-        Return Label4
+        Label4.Text = F2()
     End Function
+
 End Class
+
